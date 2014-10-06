747294e1b4d92d26c4cf26c690ac797ec758bb2e
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-10-06 15:52:38 -0400

    Updated tests.

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,11 @@
 import sys
 sys.path.append('../')
 
+
 import unittest
 
 from compound import Compound
+from capacity import CapacityCalculator
 
 """
 Using Test Driven Development (TDD), tests will be implemented using unittest to 
@@ -44,17 +46,50 @@ class TestingCompound(unittest.TestCase):
         cpd    = Compound(self.test_string3)
         self.assertEqual(cpd.composition_dict, result)
 
-    def test_compound_composition2(self):
-        result = { 'Na' : 1.1, 'Fe' : 2.2, 'P' : 3.3, 'O' : 4.4}
-        cpd    = Compound(self.test_string4)
-        self.assertEqual(cpd.composition_dict, result)
-
 
     def test_compound_oxidation_states1(self):
-        result = []
-        cpd    = Compound(self.test_string4)
-        self.assertEqual(cpd.list_oxidation_states, result)
+        result = { 'Na' : 1., 'Fe' : 2., 'P' : 5., 'O' : -2.} 
+        cpd    = Compound(self.test_string3)
+        self.assertEqual(cpd.oxidation_states_dict, result)
 
+    def test_compound_oxidation_states2(self):
+        # Construction to insure we catch the error
+        result = None
+        cpd = Compound(self.test_string4)
+
+        self.assertEqual(cpd.oxidation_states_dict, result)
+
+class TestingCapacityCalculator(unittest.TestCase):
+
+
+    def setUp(self):
+
+
+        self.tol = 1e-12
+        test_string1 = 'Na Fe P O4'
+
+        test_string2 = 'Na2 Fe O2'
+
+        active_ion = 'Na'
+
+        self.CC = CapacityCalculator(active_ion)
+
+        self.cpd1 = Compound(test_string1)
+        self.cpd2 = Compound(test_string2)
+
+
+    def test_capacity_calculator_1(self):
+        tol = 1e-12
+        result = 154.20331925588297
+        test  = self.CC.get_capacity(self.cpd1)
+
+        self.assertTrue(abs(test-result) < self.tol)
+
+    def test_capacity_calculator_2(self):
+        result = 200.27509878916402
+        test  = self.CC.get_capacity(self.cpd2)
+
+        self.assertTrue(abs(test-result) < self.tol)
 
 
 if __name__ == '__main__':
